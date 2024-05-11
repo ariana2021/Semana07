3b89bd29d81916b8ff9d8d063d811a32411d8906
Alumno <Alumno@LAB1504-04>
2024-05-11 16:31:11 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/ConecctionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ConnectionString
+    {
+        private string connectionString = "Data Source=LAB1504-04\\\\SQLEXPRESS1;Initial Catalog=FacturaDB;User Id=ariana;Password=123456";
+    }
+}
